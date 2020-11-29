8e54ae2e3b4f6c6b84c47e8350c34b2c16a87969
Konstantin060290 <73125531+Konstantin060290@users.noreply.github.com>
2020-11-29 23:34:51 +0300

    FirstRelease

---
+++ ITMO.WFCS.L2U6-9/ITMO.WFCS.L2U6/Magazine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITMO.WFCS.L2U6
+{
+    class Magazine : Item, IPubs
+    {
+        private String volume;    // том
+        private int number;        // номер
+        private String title;       // название
+        private int year;      // дата выпуска
+        private bool returnSrok;
+        public bool IfSubs { get; set; } // подписка на журнал
+
+        public Magazine(String volume, int number, String title, int year, long invNumber, bool taken, bool IfSubs) : base(invNumber, taken)
+        {
+            this.volume = volume;
+            this.number = number;
+            this.title = title;
+            this.year = year;
+            this.IfSubs = IfSubs;
+        }
+
+        public void ReturnSrok()
+        {
+            returnSrok = true;
+        }
+        public override void Return()    // операция "вернуть"
+       {
+            //taken = true;
+            if (returnSrok == true)
+                taken = true;
+            else
+                taken = false;
+        }
+
+        // реализация интерфейса
+
+      
+
+      public void Subs()
+      {
+           
+      }
+
+
+       public override string ToString()
+       {
+           if (IfSubs)
+           return "\nЖурнал:\n Название: " + title + "\nТом: " + volume +
+           "\n Номер: " + number + "\nГод выпуска: " + year + "\n Подписка оформлена"+ base.ToString();
+           else
+               return "\nЖурнал:\n Название: " + title + "\nТом: " + volume +
+           "\n Номер: " + number + "\nГод выпуска: " + year + "\n Подписка не оформлена"+ base.ToString(); ;
+       }
+    }
+}
